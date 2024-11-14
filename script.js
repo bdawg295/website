87e16c6f45cf599b3f272b172839cb71d08539af
Brandon <bmw7216@rit.edu>
2024-11-14 18:52:38 -0500

    ob

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 function learnMore() {
     window.location.href = "#about";
   }
-  
-  // Check if styles.css and script.js are being directly accessed
-const verifyFileAccess = (file) => {
-  fetch(file, { method: 'HEAD' })
-    .then((response) => {
-      if (!response.ok) {
-        // Redirect if the request is invalid
-        window.location.href = '/';
-      }
-    })
-    .catch((error) => {
-      console.error('Error verifying access:', error);
-      window.location.href = '/'; // Redirect on error
-    });
-};
 
-// Add checks for both files
-verifyFileAccess('styles.css');
-verifyFileAccess('script.js');
+  if (document.referrer === '') {
+    // Redirect if the file is being accessed directly
+    window.location.href = '/';
+  }
+  
+//   // Check if styles.css and script.js are being directly accessed
+// const verifyFileAccess = (file) => {
+//   fetch(file, { method: 'HEAD' })
+//     .then((response) => {
+//       if (!response.ok) {
+//         // Redirect if the request is invalid
+//         window.location.href = '/';
+//       }
+//     })
+//     .catch((error) => {
+//       console.error('Error verifying access:', error);
+//       window.location.href = '/'; // Redirect on error
+//     });
+// };
+
+// // Add checks for both files
+// verifyFileAccess('styles.css');
+// verifyFileAccess('script.js');
