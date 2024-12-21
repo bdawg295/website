1a4727033422fb00f61bb7c2d238b12a3cc8429b
Brandon <bmw7216@rit.edu>
2024-12-20 18:45:32 -0800

    a

--- script.js
+++ script.js
@@ -2,21 +2,3 @@ function learnMore() {
     window.location.href = "#about";
   }
   
-//   // Check if styles.css and script.js are being directly accessed
-// const verifyFileAccess = (file) => {
-//   fetch(file, { method: 'HEAD' })
-//     .then((response) => {
-//       if (!response.ok) {
-//         // Redirect if the request is invalid
-//         window.location.href = '/';
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Error verifying access:', error);
-//       window.location.href = '/'; // Redirect on error
-//     });
-// };
-
-
-// verifyFileAccess('styles.css');
-// verifyFileAccess('script.js');
